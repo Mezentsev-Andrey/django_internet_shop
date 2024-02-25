b4e87912e5a7912668b19157c60c1d21a0323065
Andrey <mezentsew@mail.ru>
2024-02-25 13:22:53 +0300

    Added classes to models.py.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'Category({self.name})'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена', **NULLABLE)
+    created_at = models.DateField(verbose_name='Дата создания', **NULLABLE)
+    updated_at = models.DateField(verbose_name='Дата изменения', **NULLABLE)
+    manufactured_at = models.DateField(verbose_name='Дата производства продукта', **NULLABLE)
+
+    def __str__(self):
+        return f'Product({self.name})'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
