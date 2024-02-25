2d7bf8a75d7134c85a10934b83d0c634c8421f6f
Andrey <mezentsew@mail.ru>
2024-02-25 15:41:22 +0300

    Commit after second migration.

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена', **NULLABLE)
     created_at = models.DateField(verbose_name='Дата создания', **NULLABLE)
     updated_at = models.DateField(verbose_name='Дата изменения', **NULLABLE)
+    manufactured_at = models.DateField(verbose_name='Дата производства продукта', **NULLABLE)
 
     def __str__(self):
         return f'Product({self.name})'
