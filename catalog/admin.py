97e6008c236885e00a37aeae4d24baa2946e5340
Andrey <mezentsew@mail.ru>
2024-02-26 16:49:42 +0300

    Added new changed information.

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,6 +6,8 @@ from catalog.models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
+    list_filter = ('name',)
+    search_fields = ('name', 'description',)
 
 
 @admin.register(Product)
