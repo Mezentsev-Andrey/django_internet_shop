a06b604cf24cd418c3cbf58775c8124e18baf32e
Andrey <mezentsew@mail.ru>
2024-03-03 17:13:55 +0300

    Added new changed information.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.views import index, contacts, product_info, product_list
 
+app_name = 'catalog_product'
 
 urlpatterns = [
     path('', index),
-    path('catalog/contacts/', contacts)
+    path('catalog/contacts/', contacts),
+    path('', product_list, name='all_products'),
+    path('<int:pk>/', product_info,name='product_info'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/index.html')
 
 
 def contacts(request):
@@ -12,3 +14,18 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'You have new message from {name} ({email}): {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def product_list(request):
+    list_products = Product.objects.all()
+    context_data = {
+        'products': list_products
+    }
+    return render(request, 'catalog/index.html', context=context_data)
+
+
+def product_info(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk),
+    }
+    return render(request, 'catalog/product_info.html', context=context)
